8768a41b50f07f8123370c2854f0da75fc9c5ad2
Cameron Derwin <camderwin@gmail.com>
2024-11-13 09:13:37 -0700

    Redesign navbar and eliminate bootstrap/tabler use in App.vue

--- sno-ui/src/App.vue
+++ sno-ui/src/App.vue
@@ -5,40 +5,36 @@ import IconSnowflake from './components/icons/IconSnowflake.vue'
 </script>
 
 <template>
-  <header class="navbar navbar-expand-sm navbar-light d-print-none">
-    <div class="container-xl">
-      <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
-        <router-link :to="{ name: 'home' }"><IconSnowflake /> sno</router-link>
-      </h1>
+  <header class="cd-navbar">
+    <span class="cd-navbar-logo">
+      <router-link :to="{ name: 'home' }"><IconSnowflake /> sno</router-link>
+    </span>
 
-      <div class="d-flex collapse navbar-collapse justify-content-center" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'weather_stations' }"
-              >Weather Stations</router-link
-            >
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'forecasts' }">Forecasts</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'mapping' }">Mapping</router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="justify-content-right">
-        <span class="navbar-text">
-          made w <IconHeart /> by <a href="https://cderwin.github.io/">cameron</a>
-        </span>
-      </div>
-    </div>
+    <ul class="cd-navbar-routes">
+      <li>
+        <router-link class="nav-link" :to="{ name: 'weather_stations' }">
+          Weather Stations
+        </router-link>
+      </li>
+      <li>
+        <router-link class="nav-link" :to="{ name: 'forecasts' }">Forecasts</router-link>
+      </li>
+      <li>
+        <router-link class="nav-link" :to="{ name: 'mapping' }">Mapping</router-link>
+      </li>
+    </ul>
+
+    <span class="cd-navbar-attribution">
+      made w <IconHeart /> by <a href="https://cderwin.github.io/">cameron</a>
+    </span>
   </header>
 
-  <div class="container-xl"><RouterView /></div>
+  <div class="content"><RouterView /></div>
 </template>
 
 <style>
 :root {
+  --cd-navbar-responsive-size: 600px;
   --cd-navbar-border-width: 1px;
   --cd-navbar-border-color: #e6e7e9;
 }
@@ -50,7 +46,7 @@ import IconSnowflake from './components/icons/IconSnowflake.vue'
   justify-content: space-between;
 
   position: relative;
-  padding: 0.25rem 0rem;
+  padding: 0.25rem 5rem;
   min-height: 3.5rem;
 
   color: #1d273b;
@@ -58,28 +54,83 @@ import IconSnowflake from './components/icons/IconSnowflake.vue'
   box-shadow: inset 0 calc(-1 * var(--cd-navbar-border-width)) 0 0 var(--cd-navbar-border-color);
 }
 
-@media (min-width: 576px) {
+@media (min-width: 600px) {
   .cd-navbar {
     flex-wrap: nowrap;
     justify-content: space-between;
   }
 }
 
-.nav-item .active {
-  /* border-bottom: 3px solid hsla(202, 79%, 30%, 0.89); */
+.cd-navbar-routes {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  list-style: none;
+  margin-bottom: 0rem;
+
+  & a {
+    padding: 0.5rem 1rem;
+    border-radius: 5%;
+  }
+}
+
+@media (min-width: 600px) {
+  .cd-navbar-routes {
+    flex-wrap: nowrap;
+    justify-content: center;
+    gap: 2rem;
+  }
+}
+
+.cd-navbar-logo {
+  a,
+  a:hover,
+  a:focus {
+    color: #247ba0;
+    text-decoration: none;
+  }
+}
+
+.cd-navbar-attribution {
+  padding: 0.5rem 1rem;
+
+  a,
+  a:hover,
+  a:focus {
+    color: #247ba0;
+  }
+}
+
+.active {
   border-bottom: 3px solid var(--color-text);
 }
 
-.navbar a {
-  text-decoration: none;
-  color: var(--color-text);
-  transition: 0.4s;
-  padding: 3px;
+@media (hover: hover) {
+  .cd-navbar-routes a:hover {
+    background-color: hsla(202, 71%, 48%, 0.2);
+  }
 }
 
-@media (hover: hover) {
-  .navbar a:hover {
-    background-color: hsla(202, 71%, 48%, 0.2);
+.content {
+  width: 100%;
+  padding-left: 0.625rem;
+  padding-right: 0.625rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@media (min-width: 1200px) {
+  .content {
+    max-width: 1140px;
+  }
+}
+
+@media (min-width: 1400px) {
+  .content {
+    max-width: 1320px;
   }
 }
 </style>
